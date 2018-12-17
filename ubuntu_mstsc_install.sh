2955fcdd45e1e849d71fb45898a073c77547295d
Da Min Mun <vmfn0401@gmail.com>
2018-12-17 16:56:17 +0900

    Update ubuntu_mstsc_install.sh

--- ubuntu_mstsc_install.sh
+++ ubuntu_mstsc_install.sh
@@ -1,6 +1,6 @@
 ## install mstsc
 ## sudo vim /etc/xrdp/xrdp.ini >> 원격 포트 수정
-sudo apt-get install gnome-session gdm3 tasksel xrdp ssh
+sudo apt-get install gnome-session xrdp ssh
 sudo apt install ubuntu-gnome-desktop
 
 sudo sed -i.bak '/fi/a #xrdp multi-users \n "gnome-session --session=gnome-flashback-metacity --disable-acceleration-check & gnome-panel" \n' /etc/xrdp/startwm.sh
@@ -13,5 +13,3 @@ sudo service xrdp restart
 ## 사이드 바
 ## 설치 후 확장 배너에서 어플리케이션 on
 sudo apt-get install gnome-tweaks
-
-sudo service gdm3 start
